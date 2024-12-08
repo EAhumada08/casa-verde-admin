43e253d0259747ba7b82af6af177859441986023
EAhumada08 <emaahmc08@gmail.com>
2024-12-07 20:09:37 -0600

    Mostrar datos de sombreros y Menu

--- src/app/dashboard/features/productos/productos-page/productos-page.component.ts
+++ src/app/dashboard/features/productos/productos-page/productos-page.component.ts
@@ -1,27 +1,45 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { AgregarProductoComponent } from '../agregar-producto/agregar-producto.component';
 import { initFlowbite } from 'flowbite';
 import { SombrerosServices } from '../sombreros.service';
+import { SombrerosStore } from '../sombreros.store';
 
 @Component({
   selector: 'app-productos-page',
   standalone: true,
   imports: [AgregarProductoComponent],
+  providers:[SombrerosStore],
   templateUrl: './productos-page.component.html',
+  changeDetection: ChangeDetectionStrategy.Default,
   styles: ``
 })
 export class ProductosPageComponent implements OnInit {
   sombreroService = inject(SombrerosServices);
-
-  sombreros: any;
+  readonly store = inject(SombrerosStore);
+  //sombreros: any;
 
   ngOnInit(): void {
     initFlowbite();
+    this.store.loadSombreros();
+    
   }
 
   constructor(){
-    this.sombreroService.getSombreros().subscribe( (data)=> {
+    /*this.sombreroService.getSombreros().subscribe( (data)=> {
       this.sombreros = data;
-    });
+    });*/
+    console.log(this.store.sombreros())
+    
+  }
+
+  delete(id:string){
+    this.sombreroService.delete(id).subscribe((res)=>{
+      console.log(res);
+      this.store.loadSombreros()
+    })
+  }
+
+  onChildEvt(){
+    this.store.loadSombreros();
   }
 }
